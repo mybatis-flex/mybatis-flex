b7e72a98ad54c86c1a9d59254e676f494c9a3993
开源海哥 <fuhai999@gmail.com>
2023-03-15 12:04:57 +0800

    add fastjson gson jackson TypeHandler

---
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/handler/BaseJsonTypeHandler.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mybatisflex.core.handler;
+
+import com.mybatisflex.core.util.StringUtil;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public abstract class BaseJsonTypeHandler<T> extends BaseTypeHandler<T> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, T parameter, JdbcType jdbcType) throws SQLException {
+        ps.setString(i, toJson(parameter));
+    }
+
+    @Override
+    public T getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        final String json = rs.getString(columnName);
+        return StringUtil.isBlank(json) ? null : parseJson(json);
+    }
+
+    @Override
+    public T getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        final String json = rs.getString(columnIndex);
+        return StringUtil.isBlank(json) ? null : parseJson(json);
+    }
+
+    @Override
+    public T getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        final String json = cs.getString(columnIndex);
+        return StringUtil.isBlank(json) ? null : parseJson(json);
+    }
+
+    protected abstract T parseJson(String json);
+
+    protected abstract String toJson(T object);
+}
---
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/handler/JacksonTypeHandler.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mybatisflex.core.handler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mybatisflex.core.exception.FlexExceptions;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+
+import java.io.IOException;
+
+@MappedTypes({Object.class})
+@MappedJdbcTypes(JdbcType.VARCHAR)
+public class JacksonTypeHandler extends BaseJsonTypeHandler<Object> {
+
+    private static ObjectMapper objectMapper;
+    private final Class<?> type;
+
+    public JacksonTypeHandler(Class<?> type) {
+        this.type = type;
+    }
+
+    @Override
+    protected Object parseJson(String json) {
+        try {
+            return getObjectMapper().readValue(json, type);
+        } catch (IOException e) {
+            throw FlexExceptions.wrap(e, "Can not parseJson by JacksonTypeHandler: " + json);
+        }
+    }
+
+    @Override
+    protected String toJson(Object object) {
+        try {
+            return getObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw FlexExceptions.wrap(e, "Can not convert object to Json by JacksonTypeHandler: " + object);
+        }
+    }
+
+    public static ObjectMapper getObjectMapper() {
+        if (null == objectMapper) {
+            objectMapper = new ObjectMapper();
+        }
+        return objectMapper;
+    }
+
+    public static void setObjectMapper(ObjectMapper objectMapper) {
+        JacksonTypeHandler.objectMapper = objectMapper;
+    }
+}
