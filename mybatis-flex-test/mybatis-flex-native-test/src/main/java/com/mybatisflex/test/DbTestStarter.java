42046c856559f4c7d291415dab1278820fa76182
开源海哥 <fuhai999@gmail.com>
2023-03-21 09:29:33 +0800

    add DbTestStarter.java

---
+++ mybatis-flex-test/mybatis-flex-native-test/src/main/java/com/mybatisflex/test/DbTestStarter.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mybatisflex.test;
+
+import com.mybatisflex.core.MybatisFlexBootstrap;
+import com.mybatisflex.core.row.Db;
+import com.mybatisflex.core.row.Row;
+import com.mybatisflex.core.row.RowKey;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+import java.util.Date;
+import java.util.List;
+
+public class DbTestStarter {
+
+    public static void main(String[] args) {
+        DataSource dataSource = new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("schema.sql")
+                .addScript("data.sql")
+                .build();
+
+        MybatisFlexBootstrap.getInstance()
+                .setDataSource(dataSource)
+                .start();
+
+        //查询全部
+        List<Row> rows = Db.selectAll("tb_account");
+        System.out.println(rows);
+
+
+        //插入 1 条数据
+        Row row = Row.ofKey(RowKey.ID_AUTO);
+        row.set("user_name", "michael yang");
+        row.set("age", 18);
+        row.set("birthday", new Date());
+        Db.insertRow("tb_account", row);
+
+        //查看刚刚插入数据的主键 id
+        System.out.println(">>>>>>>>>id: " + row.get("id"));
+
+
+        //再次查询全部数据
+        rows = Db.selectAll("tb_account");
+        System.out.println(rows);
+    }
+}
