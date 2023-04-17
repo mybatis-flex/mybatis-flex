98aca5bf6ecf672b4ab6dddb7c1f3352b6f10c2e
开源海哥 <fuhai999@gmail.com>
2023-04-17 10:57:49 +0800

    v1.1.4 release (^.^)YYa!!

--- mybatis-flex-core/src/main/java/com/mybatisflex/core/FlexConsts.java
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/FlexConsts.java
@@ -20,7 +20,7 @@ package com.mybatisflex.core;
  */
 public class FlexConsts {
     public static final String NAME = "MyBatis-Flex";
-    public static final String VERSION = "1.1.3";
+    public static final String VERSION = "1.1.4";
 
     public static final String DEFAULT_PRIMARY_FIELD = "id";
 
