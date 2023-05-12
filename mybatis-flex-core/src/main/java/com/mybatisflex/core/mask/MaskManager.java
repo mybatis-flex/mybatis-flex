15ebdce681d1e72d6a8271e49d9de399b3a1bb71
庄佳彬 <zhuangjiabin@jrizju.com>
2023-05-12 11:15:03 +0800

    processer->processor

--- mybatis-flex-core/src/main/java/com/mybatisflex/core/mask/MaskManager.java
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/mask/MaskManager.java
@@ -26,21 +26,21 @@ public class MaskManager {
 
 
     /**
-     * 脱敏处理器，type : processer
+     * 脱敏处理器，type : processor
      */
-    private static Map<String, MaskProcessor> processerMap = new HashMap<>();
+    private static final Map<String, MaskProcessor> processorMap = new HashMap<>();
 
 
     static {
-        registerMaskProcesser(Masks.MOBILE, Masks.MOBILE_PROCESSOR);
-        registerMaskProcesser(Masks.FIXED_PHONE, Masks.FIXED_PHONE_PROCESSOR);
-        registerMaskProcesser(Masks.ID_CARD_NUMBER, Masks.ID_CARD_NUMBER_PROCESSOR);
-        registerMaskProcesser(Masks.CHINESE_NAME, Masks.CHINESE_NAME_PROCESSOR);
-        registerMaskProcesser(Masks.ADDRESS, Masks.ADDRESS_PROCESSOR);
-        registerMaskProcesser(Masks.EMAIL, Masks.EMAIL_PROCESSOR);
-        registerMaskProcesser(Masks.PASSWORD, Masks.PASSWORD_PROCESSOR);
-        registerMaskProcesser(Masks.CAR_LICENSE, Masks.CAR_LICENSE_PROCESSOR);
-        registerMaskProcesser(Masks.BANK_CARD_NUMBER, Masks.BANK_CARD_PROCESSOR);
+        registerMaskProcessor(Masks.MOBILE, Masks.MOBILE_PROCESSOR);
+        registerMaskProcessor(Masks.FIXED_PHONE, Masks.FIXED_PHONE_PROCESSOR);
+        registerMaskProcessor(Masks.ID_CARD_NUMBER, Masks.ID_CARD_NUMBER_PROCESSOR);
+        registerMaskProcessor(Masks.CHINESE_NAME, Masks.CHINESE_NAME_PROCESSOR);
+        registerMaskProcessor(Masks.ADDRESS, Masks.ADDRESS_PROCESSOR);
+        registerMaskProcessor(Masks.EMAIL, Masks.EMAIL_PROCESSOR);
+        registerMaskProcessor(Masks.PASSWORD, Masks.PASSWORD_PROCESSOR);
+        registerMaskProcessor(Masks.CAR_LICENSE, Masks.CAR_LICENSE_PROCESSOR);
+        registerMaskProcessor(Masks.BANK_CARD_NUMBER, Masks.BANK_CARD_PROCESSOR);
     }
 
 
@@ -48,10 +48,10 @@ public class MaskManager {
      * 注册处理器，用户可以注册新的脱敏处理器 或者 覆盖内置的处理器
      *
      * @param type      处理器类型
-     * @param processer 脱敏处理器
+     * @param processor 脱敏处理器
      */
-    public static void registerMaskProcesser(String type, MaskProcessor processer) {
-        processerMap.put(type, processer);
+    public static void registerMaskProcessor(String type, MaskProcessor processor) {
+        processorMap.put(type, processor);
     }
 
 
@@ -91,9 +91,9 @@ public class MaskManager {
             return data;
         }
 
-        MaskProcessor maskProcessor = processerMap.get(type);
+        MaskProcessor maskProcessor = processorMap.get(type);
         if (maskProcessor == null) {
-            throw new IllegalStateException("Can not get mask processer for by type: " + type);
+            throw new IllegalStateException("Can not get mask processor for by type: " + type);
         }
 
         return maskProcessor.mask(data);
