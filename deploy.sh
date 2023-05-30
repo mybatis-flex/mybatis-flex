ede1636c94562e16230835432b602740e344fa28
开源海哥 <fuhai999@gmail.com>
2023-05-30 17:34:53 +0800

    v1.3.3 release (^.^)YYa!!

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-# gpgconf --kill gpg-agent
- mvn deploy -pl "parent,mybatis-flex-annotation,mybatis-flex-codegen,mybatis-flex-core,mybatis-flex-spring,mybatis-flex-spring-boot-starter" -e -P release
