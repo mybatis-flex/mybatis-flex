32d45dcac956bc4e5d4212f2da0dc1cea399ac81
Suomm <1474983351@qq.com>
2023-06-06 13:01:43 +0800

    style: 工具类添加私有构造器。

--- mybatis-flex-core/src/main/java/com/mybatisflex/core/util/ArrayUtil.java
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/util/ArrayUtil.java
@@ -1,17 +1,17 @@
-/**
- * Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+ *  Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.mybatisflex.core.util;
 
@@ -20,13 +20,15 @@ import java.util.Objects;
 
 public class ArrayUtil {
 
+    private ArrayUtil() {}
+
 
     /**
      * 判断数组是否为空
      *
-     * @param array
-     * @param <T>
-     * @return 空 true
+     * @param array 数组
+     * @param <T>   数组类型
+     * @return {@code true} 数组为空，{@code false} 数组不为空
      */
     public static <T> boolean isEmpty(T[] array) {
         return array == null || array.length == 0;
@@ -36,9 +38,9 @@ public class ArrayUtil {
     /**
      * 判断数组是否不为空
      *
-     * @param array
-     * @param <T>
-     * @return
+     * @param array 数组
+     * @param <T>   数组类型
+     * @return {@code true} 数组不为空，{@code false} 数组为空
      */
     public static <T> boolean isNotEmpty(T[] array) {
         return !isEmpty(array);
