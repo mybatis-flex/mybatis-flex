543803aefb4365f2c26778e529ec4f73de7f3a96
Suomm <1474983351@qq.com>
2023-06-19 11:14:15 +0800

    style: 更新 Listener 注释，添加版权信息。

--- mybatis-flex-annotation/src/main/java/com/mybatisflex/annotation/Listener.java
+++ mybatis-flex-annotation/src/main/java/com/mybatisflex/annotation/Listener.java
@@ -1,7 +1,23 @@
+/*
+ *  Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.mybatisflex.annotation;
 
 /**
- * 监听器
+ * 监听器接口。
  *
  * @author snow
  * @since 2023/4/28
@@ -9,8 +25,9 @@ package com.mybatisflex.annotation;
 public interface Listener extends Comparable<Listener> {
 
     /**
-     * 多个监听器时的执行顺序
-     * <p>值越小越早触发执行</p>
+     * <p>多个监听器时的执行顺序。
+     *
+     * <p>值越小越早触发执行。
      *
      * @return order
      */
@@ -22,4 +39,5 @@ public interface Listener extends Comparable<Listener> {
     default int compareTo(Listener other) {
         return order() - other.order();
     }
+
 }
