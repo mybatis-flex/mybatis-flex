002feafd5260951f59930d515809c6b25565e0c4
Suomm <1474983351@qq.com>
2023-06-19 11:16:13 +0800

    style: 更新 UseDataSource 注释。

--- mybatis-flex-annotation/src/main/java/com/mybatisflex/annotation/UseDataSource.java
+++ mybatis-flex-annotation/src/main/java/com/mybatisflex/annotation/UseDataSource.java
@@ -1,31 +1,33 @@
-/**
- * Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+ *  Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.mybatisflex.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * 多数据源切换注解。
+ */
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface UseDataSource {
 
     /**
-     * 使用哪个数据源，配置的是 Mybatis 的 environmentId
+     * 使用哪个数据源，配置的是 Mybatis 的 environmentId 属性。
      */
     String value();
-
 
 }
