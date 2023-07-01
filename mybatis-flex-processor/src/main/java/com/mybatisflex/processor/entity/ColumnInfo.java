2873c6a2bc966c20157c7e13a97b3fb227ca854c
Suomm <1474983351@qq.com>
2023-07-01 22:53:12 +0800

    feat: 添加 ColumnInfo 实体类封装列信息。

---
+++ mybatis-flex-processor/src/main/java/com/mybatisflex/processor/entity/ColumnInfo.java
@@ -0,0 +1,66 @@
+/*
+ *  Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.mybatisflex.processor.entity;
+
+/**
+ * 列详细信息。
+ *
+ * @author 王帅
+ * @since 2023-07-01
+ */
+public class ColumnInfo {
+
+    /**
+     * 属性名。
+     */
+    private String property;
+
+    /**
+     * 列名。
+     */
+    private String column;
+
+    /**
+     * 别名。
+     */
+    private String alias;
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+}
