82d6d4b8354487a050a61574ebf9a3f1c74d2123
开源海哥 <fuhai999@gmail.com>
2023-07-05 16:29:46 +0800

    使用 junit 重构测试模块

---
+++ mybatis-flex-test/mybatis-flex-native-test/src/main/java/com/mybatisflex/test/AccountTester.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mybatisflex.test;
+
+import com.mybatisflex.core.MybatisFlexBootstrap;
+import com.mybatisflex.core.audit.AuditManager;
+import com.mybatisflex.core.audit.ConsoleMessageCollector;
+import com.mybatisflex.core.audit.MessageCollector;
+import com.mybatisflex.core.query.If;
+import com.mybatisflex.core.query.QueryWrapper;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+
+public class AccountTester {
+
+    static AccountMapper accountMapper;
+
+    @BeforeClass
+    public static void init() {
+        DataSource dataSource = new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("schema.sql")
+                .addScript("data.sql")
+                .build();
+
+        MybatisFlexBootstrap bootstrap = MybatisFlexBootstrap.getInstance()
+                .setDataSource(dataSource)
+                .addMapper(AccountMapper.class)
+                .start();
+
+        //开启审计功能
+        AuditManager.setAuditEnable(true);
+
+        //设置 SQL 审计收集器
+        MessageCollector collector = new ConsoleMessageCollector();
+        AuditManager.setMessageCollector(collector);
+
+
+        accountMapper = bootstrap.getMapper(AccountMapper.class);
+    }
+
+
+    @Test
+    public void testLambda() {
+        QueryWrapper queryWrapper = QueryWrapper.create();
+        queryWrapper.where(Account::getId).ge(100)
+                .and(Account::getUserName).like("michael")
+                .or(Account::getUserName).like(null, If::notNull);
+        System.out.println(queryWrapper.toSQL());
+    }
+
+
+    @Test
+    public void testSelectAsToDTO() {
+        List<AccountDTO> accountDTOS = accountMapper.selectListByQueryAs(QueryWrapper.create(), AccountDTO.class);
+        System.out.println(accountDTOS);
+    }
+
+
+}
