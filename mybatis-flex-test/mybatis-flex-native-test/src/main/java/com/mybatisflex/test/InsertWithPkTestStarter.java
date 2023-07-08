78508146480cbd34a19ad53735219aafab6ccf0a
Suomm <1474983351@qq.com>
2023-07-08 11:20:31 +0800

    test: 添加断言。

--- mybatis-flex-test/mybatis-flex-native-test/src/main/java/com/mybatisflex/test/InsertWithPkTestStarter.java
+++ mybatis-flex-test/mybatis-flex-native-test/src/main/java/com/mybatisflex/test/InsertWithPkTestStarter.java
@@ -1,17 +1,17 @@
-/**
- * Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+ *  Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.mybatisflex.test;
 
@@ -20,6 +20,7 @@ import com.mybatisflex.core.audit.AuditManager;
 import com.mybatisflex.core.audit.ConsoleMessageCollector;
 import com.mybatisflex.core.audit.MessageCollector;
 import com.mybatisflex.core.logicdelete.LogicDeleteManager;
+import org.junit.Assert;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -59,10 +60,9 @@ public class InsertWithPkTestStarter {
         Account account = new Account();
         account.setId(1L);
         account.setUserName("michael1");
-        accountMapper.insertWithPk(account);
+        int rows = accountMapper.insertWithPk(account);
+        Assert.assertEquals(1, rows);
         accountMapper.selectAll().forEach(System.out::println);
-
-
-
     }
+
 }
