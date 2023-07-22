0d31f43337ca3f2180994c41a43badf115c552cc
Suomm <1474983351@qq.com>
2023-07-22 13:58:45 +0800

    feat: 添加链式查询包装器。

---
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/query/QueryWrapperChain.java
@@ -0,0 +1,96 @@
+/*
+ *  Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.mybatisflex.core.query;
+
+import com.mybatisflex.core.BaseMapper;
+import com.mybatisflex.core.paginate.Page;
+import com.mybatisflex.core.util.SqlUtil;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * {@link QueryWrapper}链式调用。
+ *
+ * @author 王帅
+ * @since 2023-07-22
+ */
+public class QueryWrapperChain<T> extends QueryWrapperAdapter<QueryWrapperChain<T>> {
+
+    private final BaseMapper<T> baseMapper;
+
+    public QueryWrapperChain(BaseMapper<T> baseMapper) {
+        this.baseMapper = baseMapper;
+    }
+
+    public static <E> QueryWrapperChain<E> create(BaseMapper<E> baseMapper) {
+        return new QueryWrapperChain<>(baseMapper);
+    }
+
+    public boolean remove() {
+        return SqlUtil.toBool(baseMapper.deleteByQuery(this));
+    }
+
+    public boolean update(T entity) {
+        return SqlUtil.toBool(baseMapper.updateByQuery(entity, this));
+    }
+
+    public boolean update(T entity, boolean ignoreNulls) {
+        return SqlUtil.toBool(baseMapper.updateByQuery(entity, ignoreNulls, this));
+    }
+
+    public long count() {
+        return baseMapper.selectCountByQuery(this);
+    }
+
+    public boolean exists() {
+        return SqlUtil.toBool(count());
+    }
+
+    public T one() {
+        return baseMapper.selectOneByQuery(this);
+    }
+
+    public <R> R oneAs(Class<R> asType) {
+        return baseMapper.selectOneByQueryAs(this, asType);
+    }
+
+    public Optional<T> oneOpt() {
+        return Optional.ofNullable(baseMapper.selectOneByQuery(this));
+    }
+
+    public <R> Optional<R> oneAsOpt(Class<R> asType) {
+        return Optional.ofNullable(baseMapper.selectOneByQueryAs(this, asType));
+    }
+
+    public List<T> list() {
+        return baseMapper.selectListByQuery(this);
+    }
+
+    public <R> List<R> listAs(Class<R> asType) {
+        return baseMapper.selectListByQueryAs(this, asType);
+    }
+
+    public Page<T> page(Page<T> page) {
+        return baseMapper.paginate(page, this);
+    }
+
+    public <R> Page<R> page(Page<R> page, Class<R> asType) {
+        return baseMapper.paginateAs(page, this, asType);
+    }
+
+}
