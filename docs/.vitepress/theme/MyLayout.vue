65d5c6a480d8a2567322ed6302df9dfba91c55b8
开源海哥 <fuhai999@gmail.com>
2023-08-02 11:01:59 +0800

    doc: update docs

--- docs/.vitepress/theme/MyLayout.vue
+++ docs/.vitepress/theme/MyLayout.vue
@@ -111,9 +111,9 @@ const {Layout} = DefaultTheme
             <div class="banner">
                 <a href="http://www.codeformat.cn" target="_blank"><img src="/assets/images/ad/code-format.png"></a>
             </div>
-          <div class="banner">
-            虚以待位
-          </div>
+            <div class="banner">
+                <a href="https://gitee.com/sdyunze/iotlink" target="_blank"><img src="/assets/images/ad/iotlink_20240802.png"></a>
+            </div>
           <div class="banner">
             虚以待位
           </div>
