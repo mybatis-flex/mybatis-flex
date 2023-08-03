44c8fd8d335f0bf4b0bccda8f18866993a9bb71d
开源海哥 <fuhai999@gmail.com>
2023-08-03 16:04:47 +0800

    style: update Copyright

--- mybatis-flex-core/src/main/java/com/mybatisflex/core/query/QueryTable.java
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/query/QueryTable.java
@@ -1,8 +1,17 @@
 /*
- * Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com). <p> Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You
- * may obtain a copy of the License at <p> http://www.apache.org/licenses/LICENSE-2.0 <p> Unless required by applicable law or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the
- * License.
+ *  Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.mybatisflex.core.query;
 
@@ -24,7 +33,8 @@ public class QueryTable implements CloneSupport<QueryTable> {
     protected String name;
     protected String alias;
 
-    public QueryTable() {}
+    public QueryTable() {
+    }
 
     public QueryTable(TableDef tableDef) {
         // TableDef的标识符号,0:不确定标识
@@ -111,7 +121,7 @@ public class QueryTable implements CloneSupport<QueryTable> {
     @Override
     public QueryTable clone() {
         try {
-            return (QueryTable)super.clone();
+            return (QueryTable) super.clone();
         } catch (CloneNotSupportedException e) {
             throw FlexExceptions.wrap(e);
         }
