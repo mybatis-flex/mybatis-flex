c76c00cebda637ea23d67f0215d4d7031825e4cd
life <13122192336@163.com>
2023-08-03 10:00:57 +0800

    修复issuse(https://gitee.com/mybatis-flex/mybatis-flex/issues/I7HJ4J)以及多数据源JdbcTemplate事务下使用报错，添加license

--- mybatis-flex-spring/src/main/java/com/mybatisflex/spring/FlexSpringTransaction.java
+++ mybatis-flex-spring/src/main/java/com/mybatisflex/spring/FlexSpringTransaction.java
@@ -1,20 +1,26 @@
+/*
+ *  Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.mybatisflex.spring;
 
-import com.mybatisflex.core.datasource.DataSourceKey;
 import com.mybatisflex.core.datasource.FlexDataSource;
-import com.mybatisflex.core.transaction.TransactionContext;
-import com.mybatisflex.core.transaction.TransactionalManager;
-import com.mybatisflex.core.util.StringUtil;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 import javax.sql.DataSource;
 import org.apache.ibatis.session.TransactionIsolationLevel;
 import org.apache.ibatis.transaction.Transaction;
-import org.springframework.util.StringUtils;
 
 /**
  * spring事务支持，解决issusehttps://gitee.com/mybatis-flex/mybatis-flex/issues/I7HJ4J
