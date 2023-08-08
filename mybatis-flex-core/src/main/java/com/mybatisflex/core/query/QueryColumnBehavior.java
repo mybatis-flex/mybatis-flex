a7b392c8a921b60e75773a1f4dba9eb71f241c4a
开源海哥 <fuhai999@gmail.com>
2023-08-08 10:31:47 +0800

    feat: add QueryColumnBehavior

---
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/query/QueryColumnBehavior.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.mybatisflex.core.query;
+
+import java.util.function.Function;
+
+/**
+ * @author michael
+ */
+public class QueryColumnBehavior {
+
+    /**
+     * 自定义全局的自动忽略参数的方法
+     */
+    private static Function<Object,Boolean> ignoreFunction;
+
+    /**
+     * 是否自动把 in(...) 只有 1 个参数的内容转换为相等 =
+     */
+    private static boolean smartConvertInToEquals = false;
+
+
+
+    public static Function<Object, Boolean> getIgnoreFunction() {
+        return ignoreFunction;
+    }
+
+    public static void setIgnoreFunction(Function<Object, Boolean> ignoreFunction) {
+        QueryColumnBehavior.ignoreFunction = ignoreFunction;
+    }
+
+    public static boolean isSmartConvertInToEquals() {
+        return smartConvertInToEquals;
+    }
+
+    public static void setSmartConvertInToEquals(boolean smartConvertInToEquals) {
+        QueryColumnBehavior.smartConvertInToEquals = smartConvertInToEquals;
+    }
+
+
+    static boolean shouldIgnoreValue(Object value){
+        if (ignoreFunction == null){
+            return false;
+        }
+        return ignoreFunction.apply(value);
+    }
+
+}
