44f84daaefbb325461694f81886bc2f2f6c12fa2
Suomm <1474983351@qq.com>
2023-08-09 16:20:42 +0800

    refactor: 统一测试模块。

--- mybatis-flex-test/mybatis-flex-seata-test/src/main/java/com/mybatisflex/test/service/TestService.java
+++ mybatis-flex-test/mybatis-flex-seata-test/src/main/java/com/mybatisflex/test/service/TestService.java
@@ -1,23 +1,37 @@
-package com.mybatisfle.test.mybatisflexspringbootseata.service;
+/*
+ *  Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.mybatisflex.test.service;
 
 
-import com.mybatisfle.test.mybatisflexspringbootseata.entity.AccountTbl;
-import com.mybatisfle.test.mybatisflexspringbootseata.entity.OrderTbl;
-import com.mybatisfle.test.mybatisflexspringbootseata.entity.StockTbl;
-import com.mybatisfle.test.mybatisflexspringbootseata.entity.table.AccountTblTableDef;
-import com.mybatisfle.test.mybatisflexspringbootseata.mapper.AccountTblMapper;
-import com.mybatisfle.test.mybatisflexspringbootseata.mapper.OrderTblMapper;
-import com.mybatisfle.test.mybatisflexspringbootseata.mapper.StockTblMapper;
 import com.mybatisflex.core.datasource.DataSourceKey;
 import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.test.entity.AccountTbl;
+import com.mybatisflex.test.entity.OrderTbl;
+import com.mybatisflex.test.entity.StockTbl;
+import com.mybatisflex.test.entity.table.AccountTblTableDef;
+import com.mybatisflex.test.mapper.AccountTblMapper;
+import com.mybatisflex.test.mapper.OrderTblMapper;
+import com.mybatisflex.test.mapper.StockTblMapper;
 import io.seata.core.context.RootContext;
 import io.seata.spring.annotation.GlobalTransactional;
 import org.mybatis.logging.Logger;
 import org.mybatis.logging.LoggerFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TestService {
@@ -33,12 +47,12 @@ public class TestService {
     @Autowired
     StockTblMapper stockTblMapper;
 
-//    @Transactional
+    //    @Transactional
     @GlobalTransactional
     public boolean buy() {
 //        DataSourceKey.use("accountdb");
-        LOGGER.warn(() -> "xid:"+RootContext.getXID());
-        QueryWrapper account =new QueryWrapper();
+        LOGGER.warn(() -> "xid:" + RootContext.getXID());
+        QueryWrapper account = new QueryWrapper();
         account.where(AccountTblTableDef.ACCOUNT_TBL.USER_ID.eq("1001"));
         AccountTbl accountTbl = accountTblMapper.selectOneByQuery(account);
         accountTbl.setMoney(accountTbl.getMoney() - 5);
@@ -59,4 +73,5 @@ public class TestService {
         orderTblMapper.insert(orderTbl);
         return true;
     }
+
 }
