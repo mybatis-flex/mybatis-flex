57f997e56c85c8060e692e2b88409b300c60b170
Suomm <1474983351@qq.com>
2023-08-13 12:09:27 +0800

    test: count 查询优化测试。

---
+++ mybatis-flex-core/src/test/java/com/mybatisflex/coretest/CountSqlTest.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.mybatisflex.coretest;
+
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.util.MapperUtil;
+import org.junit.Test;
+
+import static com.mybatisflex.coretest.table.AccountTableDef.ACCOUNT;
+import static com.mybatisflex.coretest.table.ArticleTableDef.ARTICLE;
+
+/**
+ * @author 王帅
+ * @since 2023-08-13
+ */
+public class CountSqlTest {
+
+    @Test
+    public void test01() {
+        QueryWrapper queryWrapper = QueryWrapper.create()
+            .select()
+            .from(ACCOUNT)
+            .leftJoin(ARTICLE).as("a1").on(ARTICLE.ACCOUNT_ID.eq(ACCOUNT.ID))
+            .leftJoin(ARTICLE).as("a2").on(ARTICLE.ACCOUNT_ID.eq(ACCOUNT.ID))
+            .where(ARTICLE.ACCOUNT_ID.in(1, 2, 3));
+
+        System.out.println(queryWrapper.toSQL());
+
+        QueryWrapper optimized = MapperUtil.optimizeCountQueryWrapper(queryWrapper);
+
+        System.out.println(optimized.toSQL());
+    }
+
+    @Test
+    public void test02() {
+        QueryWrapper queryWrapper = QueryWrapper.create()
+            .select()
+            .from(ACCOUNT)
+            .leftJoin(ARTICLE).as("a1").on(ARTICLE.ACCOUNT_ID.eq(ACCOUNT.ID))
+            .leftJoin(ARTICLE).as("a2").on(ARTICLE.ACCOUNT_ID.eq(ACCOUNT.ID))
+            .where("a1.account_id IN (1, 2, 3)");
+
+        System.out.println(queryWrapper.toSQL());
+
+        QueryWrapper optimized = MapperUtil.optimizeCountQueryWrapper(queryWrapper);
+
+        System.out.println(optimized.toSQL());
+    }
+
+    @Test
+    public void test03() {
+        QueryWrapper queryWrapper = QueryWrapper.create()
+            .select()
+            .from(ACCOUNT.as("a"))
+            .leftJoin("tb_article").as("a1").on("a1.account_id = a.id")
+            .leftJoin("tb_article").as("a2").on("a1.account_id = a.id")
+            .where("a1.account_id IN (1, 2, 3)");
+
+        System.out.println(queryWrapper.toSQL());
+
+        QueryWrapper optimized = MapperUtil.optimizeCountQueryWrapper(queryWrapper);
+
+        System.out.println(optimized.toSQL());
+    }
+
+}
