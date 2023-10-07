a40763e7b6daed0255bcf836792e72ab9546c041
开源海哥 <fuhai999@gmail.com>
2023-10-07 16:11:00 +0800

    fix: throw IllegalStateException if config "valueField" for `@Relation` annotations

--- mybatis-flex-core/src/main/java/com/mybatisflex/core/relation/AbstractRelation.java
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/relation/AbstractRelation.java
@@ -91,7 +91,9 @@ abstract class AbstractRelation<SelfEntity> {
         this.targetSchema = targetSchema;
         this.targetTable = targetTableInfo != null ? targetTableInfo.getTableName() : targetTable;
 
-        this.targetEntityClass = relationFieldWrapper.getMappingType();
+        //当指定了 valueField 的时候，一般是 String Integer 等基本数据类型
+        this.targetEntityClass = StringUtil.isNotBlank(valueField) ? targetTableInfo.getEntityClass(): relationFieldWrapper.getMappingType();
+
         this.targetField = ClassUtil.getFirstField(targetEntityClass, field -> field.getName().equals(targetField));
         this.targetFieldWrapper = FieldWrapper.of(targetEntityClass, targetField);
 
