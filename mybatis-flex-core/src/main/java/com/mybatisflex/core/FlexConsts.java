67a187484fc29d6ade10b67fe8fe12c87fa1f4a5
开源海哥 <fuhai999@gmail.com>
2023-10-09 17:43:54 +0800

    build: v1.7.0 release (^.^)YYa!!

--- mybatis-flex-core/src/main/java/com/mybatisflex/core/FlexConsts.java
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/FlexConsts.java
@@ -27,7 +27,7 @@ public class FlexConsts {
     }
 
     public static final String NAME = "MyBatis-Flex";
-    public static final String VERSION = "1.6.9";
+    public static final String VERSION = "1.7.0";
 
 
     public static final String SQL = "$$sql";
