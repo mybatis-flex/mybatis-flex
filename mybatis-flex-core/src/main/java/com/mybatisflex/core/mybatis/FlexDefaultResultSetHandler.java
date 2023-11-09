60a0fae0a071ee31e1453e0542e10469aba4736a
natsufumij <natsufumij@yeah.net>
2023-11-09 15:46:52 +0800

    修复：当构造函数字段顺序与数据库字段定义顺序不一致时可能出现的映射失败的问题

--- mybatis-flex-core/src/main/java/com/mybatisflex/core/mybatis/FlexDefaultResultSetHandler.java
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/mybatis/FlexDefaultResultSetHandler.java
@@ -763,11 +763,53 @@ public class FlexDefaultResultSetHandler extends DefaultResultSetHandler {
             ? objectFactory.create(resultType, constructorArgTypes, constructorArgs) : null;
     }
 
+    /**
+     * <h3>将驼峰转为下划线</h3>
+     * <pre>{@code
+     *  var word = underlineByhump("helloWorld")
+     *  //word == "hello_world"
+     * }</pre>
+     * @author natsufumij
+     */
+    public static String underlineByhump(String str) {
+        if (str == null) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        // 前置字符是否大写
+        boolean preCharIsUpperCase = true;
+        // 当前字符是否大写
+        boolean curreCharIsUpperCase = true;
+        // 下一字符是否大写
+        boolean nexteCharIsUpperCase = true;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (i > 0) {
+                preCharIsUpperCase = Character.isUpperCase(str.charAt(i - 1));
+            } else {
+                preCharIsUpperCase = false;
+            }
+            curreCharIsUpperCase = Character.isUpperCase(c);
+            if (i < (str.length() - 1)) {
+                nexteCharIsUpperCase = Character.isUpperCase(str.charAt(i + 1));
+            }
+            if (preCharIsUpperCase && curreCharIsUpperCase && !nexteCharIsUpperCase) {
+                sb.append("_");
+            } else if ((i != 0 && !preCharIsUpperCase) && curreCharIsUpperCase) {
+                sb.append("_");
+            }
+            sb.append(Character.toLowerCase(c));
+        }
+        return sb.toString();
+    }
+
+
     private boolean applyColumnOrderBasedConstructorAutomapping(ResultSetWrapper rsw, List<Class<?>> constructorArgTypes,
                                                                 List<Object> constructorArgs, Constructor<?> constructor, boolean foundValues) throws SQLException {
         for (int i = 0; i < constructor.getParameterTypes().length; i++) {
             Class<?> parameterType = constructor.getParameterTypes()[i];
-            String columnName = rsw.getColumnNames().get(i);
+            Parameter parameter = constructor.getParameters()[i];
+            String columnName = underlineByhump(parameter.getName());
             TypeHandler<?> typeHandler = rsw.getTypeHandler(parameterType, columnName);
             Object value = typeHandler.getResult(rsw.getResultSet(), columnName);
             constructorArgTypes.add(parameterType);
