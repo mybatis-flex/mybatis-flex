e24abffba5b66ea0c184b914884ed72bac8b60ff
noear <noear@live.cn>
2023-11-13 14:21:15 +0800

    solon 升为 2.5.3

--- mybatis-flex-solon-plugin/src/main/java/com/mybatisflex/solon/integration/XPluginImpl.java
+++ mybatis-flex-solon-plugin/src/main/java/com/mybatisflex/solon/integration/XPluginImpl.java
@@ -17,7 +17,7 @@
 package com.mybatisflex.solon.integration;
 
 import org.apache.ibatis.solon.integration.MybatisAdapterManager;
-import org.noear.solon.core.AopContext;
+import org.noear.solon.core.AppContext;
 import org.noear.solon.core.Plugin;
 
 /**
@@ -27,11 +27,9 @@ import org.noear.solon.core.Plugin;
  * @since 2.2
  */
 public class XPluginImpl implements Plugin {
-
     @Override
-    public void start(AopContext context) {
+    public void start(AppContext context) throws Throwable {
         // 此插件的 solon.plugin.priority 会大于 mybatis-solon-plugin 的值
         MybatisAdapterManager.setAdapterFactory(new MybatisAdapterFactoryFlex());
     }
-
 }
