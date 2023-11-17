addf72d5cd982843660c7b72a1f3e01b4ce52842
Suomm <1474983351@qq.com>
2023-11-17 23:40:03 +0800

    test: 添加重名映射测试。

---
+++ mybatis-flex-test/mybatis-flex-spring-boot-test/src/main/java/com/mybatisflex/test/alisa/BaseEntity.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.mybatisflex.test.alisa;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 父类。
+ *
+ * @author 王帅
+ * @since 2023-11-16
+ */
+public abstract class BaseEntity implements Serializable {
+
+    private Date createTime;
+    private String createBy;
+    private Date updateTime;
+    private String updateBy;
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+            "createTime=" + createTime +
+            ", createBy='" + createBy + '\'' +
+            ", updateTime=" + updateTime +
+            ", updateBy='" + updateBy + '\'' +
+            '}';
+    }
+
+}
