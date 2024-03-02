c60ccfba9e1391d406853533a4750b3c74deb02a
Michael Yang <fuhai999@gmail.com>
2024-03-02 11:04:22 +0800

    build: v1.8.1 release (^.^)YYa!!

--- mybatis-flex-core/src/main/java/com/mybatisflex/core/FlexConsts.java
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/FlexConsts.java
@@ -27,7 +27,7 @@ public class FlexConsts {
     }
 
     public static final String NAME = "MyBatis-Flex";
-    public static final String VERSION = "1.8.0";
+    public static final String VERSION = "1.8.1";
 
 
     public static final String SQL = "$$sql";
