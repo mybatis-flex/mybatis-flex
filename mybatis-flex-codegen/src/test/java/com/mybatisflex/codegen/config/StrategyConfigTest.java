4c7093a837bfd5d142f7f49d6bd897b4a91e4c36
Michael Yang <fuhai999@gmail.com>
2024-03-24 11:24:49 +0800

    test: optimize test

--- mybatis-flex-codegen/src/test/java/com/mybatisflex/codegen/config/StrategyConfigTest.java
+++ mybatis-flex-codegen/src/test/java/com/mybatisflex/codegen/config/StrategyConfigTest.java
@@ -15,7 +15,7 @@ public class StrategyConfigTest extends TestCase {
         boolean var1 = strategyConfig.isSupportGenerate("username");
         boolean var2 = strategyConfig.isSupportGenerate("sys_info");
         boolean var3 = strategyConfig.isSupportGenerate("db_redis_01");
-        boolean var4 = strategyConfig.isSupportGenerate("user");
+        boolean var4 = strategyConfig.isSupportGenerate("other");
         if (var1 || var2 || var3 || !var4){
             throw new RuntimeException("测试失败");
         }
