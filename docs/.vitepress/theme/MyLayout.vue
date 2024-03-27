ef05f225cf845f26d7fd6c21f01520ebce068cf1
Michael Yang <fuhai999@gmail.com>
2024-03-27 19:35:49 +0800

    doc: update docs

--- docs/.vitepress/theme/MyLayout.vue
+++ docs/.vitepress/theme/MyLayout.vue
@@ -78,8 +78,16 @@ const {Layout} = DefaultTheme
         <!--docs: https://vitepress.dev/guide/extending-default-theme#layout-slots-->
         <template #doc-before>
             <div style="margin-bottom: 30px">
-                <a href="https://mp.weixin.qq.com/s/V34VkARgBCBKdytSlKhosg" target="_blank">
+                <a href="https://mp.weixin.qq.com/s/sAe_bO4eW7dJsNgwoG0x9g" target="_blank">
                     <img src="/assets/images/ad/admin-banner.jpg">
+                </a>
+            </div>
+        </template>
+
+        <template #home-features-after>
+            <div style="display: flex; justify-content: center;margin-top: 60px">
+                <a href="https://mp.weixin.qq.com/s/sAe_bO4eW7dJsNgwoG0x9g" target="_blank" style="max-width: 90%">
+                    <img src="/assets/images/ad/admin-banner.jpg" style="width: 650px;">
                 </a>
             </div>
         </template>
