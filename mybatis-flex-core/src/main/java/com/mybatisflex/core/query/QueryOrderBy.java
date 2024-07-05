0edf12b05d495235bc879e9d61561ba5baa7ba7c
kamosama <837080904@qq.com>
2024-07-05 10:26:11 +0800

    fix: 解决orderBy时传入的变量失效问题

--- mybatis-flex-core/src/main/java/com/mybatisflex/core/query/QueryOrderBy.java
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/query/QueryOrderBy.java
@@ -29,7 +29,7 @@ import java.util.List;
  */
 public class QueryOrderBy implements CloneSupport<QueryOrderBy> {
 
-    private QueryColumn queryColumn;
+    QueryColumn queryColumn;
 
     /**
      * asc, desc
