eaf0635f34979cebe77b8979c996c01ede845baa
Michael Yang <fuhai999@gmail.com>
2024-07-29 16:39:45 +0800

    chore: 回退 https://github.com/mybatis-flex/mybatis-flex/pull/351

--- mybatis-flex-processor/src/main/java/com/mybatisflex/processor/config/MybatisFlexConfig.java
+++ mybatis-flex-processor/src/main/java/com/mybatisflex/processor/config/MybatisFlexConfig.java
@@ -50,8 +50,6 @@ public class MybatisFlexConfig {
     protected final Properties properties = new Properties();
 
     public MybatisFlexConfig(Filer filer) {
-        // 先从 resources 目录读取 mybatis-flex.config 配置文件
-        loadConfigFromResource();
         try {
             //target/classes/
             FileObject resource = filer.createResource(StandardLocation.CLASS_OUTPUT, "", "mybatis-flex");
@@ -99,20 +97,6 @@ public class MybatisFlexConfig {
         }
     }
 
-    private void loadConfigFromResource() {
-        Properties resourceProp = new Properties();
-        try (InputStream resourceStream = MybatisFlexConfig.class.getClassLoader().getResourceAsStream(APT_FILE_NAME)) {
-            if (resourceStream == null) {
-                return;
-            }
-            resourceProp.load(resourceStream);
-            if (resourceProp != null && !resourceProp.isEmpty()) {
-                properties.putAll(resourceProp);
-            }
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-    }
 
     public String get(ConfigurationKey key) {
         return properties.getProperty(key.getConfigKey(), key.getDefaultValue());
