aa85c7663557bd26fc5434d9e4a59fac786f67d7
Michael Yang <fuhai999@gmail.com>
2024-08-09 12:02:25 +0800

    fix: processor 退回到 v1.9.3

--- mybatis-flex-processor/src/main/java/com/mybatisflex/processor/builder/ContentBuilder.java
+++ mybatis-flex-processor/src/main/java/com/mybatisflex/processor/builder/ContentBuilder.java
@@ -168,7 +168,8 @@ public class ContentBuilder {
      * 构建 Tables 文件常量属性。
      */
     public static void buildTablesField(StringBuilder importBuilder, StringBuilder fieldBuilder, TableInfo tableInfo,
-                                        String tableDefClassSuffix, String tableDefPropertiesNameStyle, String tableDefInstanceSuffix, String tableDefPackage) {
+                                        String tableDefClassSuffix, String tableDefPropertiesNameStyle, String tableDefInstanceSuffix) {
+        String tableDefPackage = StrUtil.buildTableDefPackage(tableInfo.getEntityName());
         String tableDefClassName = tableInfo.getEntitySimpleName().concat(tableDefClassSuffix);
         importBuilder.append("import ").append(tableDefPackage).append('.').append(tableDefClassName).append(";\n");
         String entityComment = tableInfo.getEntityComment();
