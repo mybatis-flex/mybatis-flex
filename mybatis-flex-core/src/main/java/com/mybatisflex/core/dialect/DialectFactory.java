58dd7e8904eddde2de9fcb9f748109a3ff5cbeab
young <18860957075@163.com>
2025-01-08 07:54:00 +0000

    fix: 修复 duckdb 方言配置
    Signed-off-by: young <18860957075@163.com>

--- mybatis-flex-core/src/main/java/com/mybatisflex/core/dialect/DialectFactory.java
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/dialect/DialectFactory.java
@@ -153,7 +153,7 @@ public class DialectFactory {
             case TRINO:
                 return new CommonsDialectImpl(KeywordWrap.NONE, LimitOffsetProcessor.SQLSERVER);
             case DUCKDB:
-                return new CommonsDialectImpl(KeywordWrap.DOUBLE_QUOTATION, LimitOffsetProcessor.MYSQL);
+                return new CommonsDialectImpl(KeywordWrap.DOUBLE_QUOTATION, LimitOffsetProcessor.POSTGRESQL);
             default:
                 return new CommonsDialectImpl();
         }
