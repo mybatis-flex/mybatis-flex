acba6066bc56f250e9dd1b312afba56a90a5b276
ruansheng <chrisruans@gmail.com>
2025-07-23 18:51:32 +0800

    feat: -m core 导入Optional包依赖

--- mybatis-flex-core/src/main/java/com/mybatisflex/core/datasource/FlexDataSource.java
+++ mybatis-flex-core/src/main/java/com/mybatisflex/core/datasource/FlexDataSource.java
@@ -36,6 +36,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
